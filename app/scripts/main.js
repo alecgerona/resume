d3409b0ee4c7f0a9ba6272fb897d20bcedcebf6e
altusgerona <alecgerona@gmail.com>
2016-06-01 05:37:14 +0800

    Enable fullpage and comment functions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,8 @@
-console.log('\'Allo \'Allo!');
+//Animate the brand logos
 TweenMax.staggerFrom(".brand-logo", 1, {x:"+=2000"}, 0.2);
+
+
+//Initialize fullpage.js
+$(document).ready(function() {
+    $('#fullpage').fullpage();
+});
