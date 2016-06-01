6fdf38288c70f62a3f7722841230d3d8f8192a3e
altusgerona <alecgerona@gmail.com>
2016-06-01 17:14:12 +0800

    Add animation for Good

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,26 @@ TweenMax.staggerFrom(".brand-logo", 1, {x:"+=2000"}, 0.2);
 
 
 //Initialize fullpage.js
-$(document).ready(function() {
-    $('#fullpage').fullpage();
+$('#fullpage').fullpage({
+    autoScrolling: false,
+	fitToSection: false
+});
+
+$(function() {
+  
+  // Init ScrollMagic Controller
+  var controller = new ScrollMagic.Controller();
+
+  
+  var tween = TweenMax.fromTo("#good", 1, {x:"-100", opacity:0}, {x:"0", opacity:1});
+
+	// build scene
+	var scene = new ScrollMagic.Scene({triggerElement: "#goodScene", duration: 200})
+					.setTween(tween)
+					.addIndicators() // add indicators (requires plugin)
+					.addTo(controller);
+          
+          
+  
+  
 });
