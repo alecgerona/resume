040b3f6a71bc39d0c44c4fba8aaab09f1dc297ce
altusgerona <alecgerona@gmail.com>
2016-06-02 12:07:26 +0800

    Disable sidescrolling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,10 +17,19 @@ $(function() {
   var tween = TweenMax.fromTo("#good", 1, {x:"-100", opacity:0}, {x:"0", opacity:1});
 
 	// build scene
-	var scene = new ScrollMagic.Scene({triggerElement: "#goodScene", duration: 200})
+	var scene = new ScrollMagic.Scene({triggerElement: "#goodScene", duration: 250})
 					.setTween(tween)
 					.addIndicators() // add indicators (requires plugin)
 					.addTo(controller);
+  
+  var tween = TweenMax.fromTo("#letsbegin", 1, {x:"+100", opacity:0}, {x:"0", opacity:1});
+
+	// build scene
+	var scene = new ScrollMagic.Scene({triggerElement: "#beginTrigger", duration: 250})
+					.setTween(tween)
+					.addIndicators() // add indicators (requires plugin)
+					.addTo(controller);
+          
           
           
   
